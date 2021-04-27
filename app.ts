ffb3b2b9683cc2550eaf9430d0f0f9efd98cb80f
Philippe <Philimart21@gmail.com>
2021-04-27 16:16:40 +0100

    update

--- app.ts
+++ app.ts
@@ -1,3 +1,6 @@
-const chalk = require("chalk");
+import chalk from "chalk";
+import getNotes from "./notes";
+
+console.log(getNotes("asdfasdf"));
 
 console.log(chalk.green.bold.inverse("Hello world!"));
